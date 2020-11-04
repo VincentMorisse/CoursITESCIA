45acb2dde89d8b0a3ad65793a9a16e2cfbaf5144
Morisse Vincent <vincent.morisse9674@gmail.com>
2020-11-04 11:35:58 +0100

    Python BP 4

--- L3/Python/Scéance1/exo.py
+++ L3/Python/Scéance1/exo.py
@@ -1,3 +1,6 @@
+from math import *
+import math
+
 def checkInputIsInt() :
     try:
         value = int(input('Saissisez votre nombre : '))
@@ -63,6 +66,102 @@ def exo6():
     valueThree = int(checkInputIsInt())
     print("La valeur maximum est ", max([valueOne, valueTwo, valueThree]))
     
+def exo7() :
+    print("Exercice 7")
+    for tempValue in range(1,101) :
+        print(tempValue)
+
+def exo8() :
+    print("Exercice 8")
+    value = int(checkInputIsInt())
+    somme = 0
+    for tempValue in range(0,value+1):
+        somme = somme + tempValue
+    print("La somme de tout les entiers jusqu'à",value," est ", somme)
+    
+def exo9():
+    print("Exercice 9")
+    print("Calculer la factorielle d'un nombre")
+    value = int(checkInputIsInt())
+    produit = 1
+    for tempValue in range(1,value+1):
+        produit = produit * tempValue
+    print("La factorielle de ",value," est ", produit)
+
+def exo10() :
+    print("Exercice 10")
+    print("Calculer le périmètre à partir d'un rayon")
+    value = int(checkInputIsInt())
+    peri = 2 * math.pi * value
+    surface = value * value * math.pi
+    print("Un cercle de rayon ", value," a un périmètre de", peri, " et une surface de ", surface)
+
+def exo11() :
+    print("Exercice 11")
+    print("Afficher tout les diviseurs d'un nombre")
+    value = int(checkInputIsInt())
+    res = []
+    for tempValue in range(1, value + 1):
+        if value % tempValue == 0:
+            res.append(tempValue)
+    print("Les diviseurs de ",value,"sont",res)
+
+def exo12() :
+    print("Exercice 12")
+    print("Affiche la table de multiplication d'un nombre")
+    value = int(checkInputIsInt())
+    for tempValue in range(0,11) :
+        print(value,"x",tempValue,"=",value*tempValue)
+
+def exo12v2() :
+    print("Exercice 12v2")
+    print("Affiche les tables de multiplication")
+    for tempValue1 in range(0,11):
+        print("Table de multiplication de", tempValue1)
+        for tempValue2 in range(0,11) :
+            print(tempValue1,"x",tempValue2,"=",tempValue1*tempValue2)
+
+def exo13() :
+    print("Exercice 13")
+    print("Calcul le quotient et le reste de la division euclidienne")
+    print("Première valeur")
+    valueOne = int(checkInputIsInt())
+    print("Deuxième valeur")
+    valueTwo = int(checkInputIsInt())
+    reste = max([valueOne,valueTwo]) % min([valueOne,valueTwo])
+    quotient = max([valueOne,valueTwo]) // min([valueOne,valueTwo])
+    print("Le quotient de cette division est ",quotient,"et le reste est",reste)
+
+def exo14() :
+    print("Exercice 14")
+    print("Savoir si un nombre est un carré parfait")
+    value = int(checkInputIsInt())
+    if sqrt(value)%1==0 :
+        print(value, " est un carré parfait")
+    else :
+        print(value, " n'est pas un carré parfait")
+
+def exo15() :
+    print("Exercice 15")
+    print("Savoir si un nombre est premier")
+    value = int(checkInputIsInt())
+    res = []
+    for tempValue in range(1, value + 1):
+        if value % tempValue == 0:
+            res.append(tempValue)
+    
+    if len(res) == 2 or res == [1] :
+        print(value," est un nombre premier")
+    else :
+        print(value,"n'est pas un nombre premier")
+        
+def exo16() :
+    print("Exercice 16")
+    chaine = input("Saissisez votre chaine de caractère : ")
+    for char in chaine :
+        print(char)
 
 if __name__ == "__main__":
-    exo6()
+    exo16()
+
+
