770ded490c02f638b4c041a9ab59075128577210
Morisse Vincent <vincent.morisse9674@gmail.com>
2020-11-04 09:55:46 +0100

    Python exo2

--- L3/Python/Scéance1/exo.py
+++ L3/Python/Scéance1/exo.py
@@ -3,5 +3,22 @@ def exo1() :
     name = input('Saissisez votre nom: ')
     print('Bienvenue ' + name)
 
+def exo2() :
+    print("Exercice 2")
+    print("Première valeur")
+    valueOne = checkInputIsInt()
+    print("Deuxième valeur")
+    valueTwo = checkInputIsInt()
+    somme= int(valueOne) + int(valueTwo)
+    print("Leur somme est ", somme)
+
+def checkInputIsInt() :
+    try:
+        value = int(input('Saissisez votre nombre : '))
+        return value
+    except :
+        print("La valeur rentrée n'est pas un int")
+        checkInputIsInt()
+
 if __name__ == "__main__":
-    exo1()
+    exo2()
